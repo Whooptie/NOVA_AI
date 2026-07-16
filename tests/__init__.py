152cf77bf43a3c0eab2a860dbc9c6f3d35fb9d74
Whooptie <kevin.blyaert@gmail.com>
2026-07-16 22:04:52 +0200

    Layer 5 copleet with camera ML detection

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Test package voor Nova AI.
-
-Alle unit tests en integratietests komen hier terecht.
-Gebruik `pytest` om de volledige suite te draaien.
-"""
-
-__version__ = "0.1.0"
-__author__ = "Kevin"
